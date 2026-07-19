866e4ad9dc5b5de4d936c5edf2a4801057fdd935
R1kks <eugene.dozorov.303@gmail.com>
2026-07-19 20:18:20 +0500

    Добавлен 1 тест

--- tests/tests.c
+++ tests/tests.c
@@ -4,6 +4,22 @@
 Группа: МК - 101
 */
 
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../lib/include/args_parser.h"
+#include "../lib/include/hexdump.h"
+
+void test_args_no_required_params() {
+    Config cfg;
+    init_config(&cfg);
+    char* argv[] = {"hexdump"}; 
+    assert(parse_args(1, argv, &cfg) == -1);
+}
+
 int main() {
+    printf("--- Starting Integration Tests ---\n");
+    RUN_TEST(test_args_no_required_params);
     return 0;
 }
